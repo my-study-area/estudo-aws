749856c39687b7b46840b127e6cf29f676b083af
Adriano Avelino <adriano-st777@hotmail.com>
2023-10-29 23:06:09 -0300

    adiciona sg para service ecs

--- playlist-youtube-ecs-fargate-node/infra/ecs/sg.tf
+++ playlist-youtube-ecs-fargate-node/infra/ecs/sg.tf
@@ -15,3 +15,21 @@ resource "aws_security_group" "fna-alb-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "fna-ecs-service-sg" {
+  name   = "fna-sevice-sg"
+  vpc_id = aws_vpc.fna-vpc.id
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    # cidr_blocks = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.fna-alb-sg.id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
